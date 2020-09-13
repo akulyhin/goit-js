ca0d8254d063439973fc40edf2c2bad430367330
Artem Kulyhin <akulyhin@gmail.com>
2020-09-13 15:38:55 +0300

    Add howork 8

---
+++ hw-8/script.js
@@ -0,0 +1,47 @@
+import gallery from './gallery-items.js';
+
+
+const refs = {
+    gallery: document.querySelector('.js-gallery'),
+    modal: document.querySelector('.lightbox'),
+    closeBtn: document.querySelector('button[data-action="close-lightbox"]'),
+    modalImg: document.querySelector('img.lightbox__image')
+}
+
+    refs.gallery.addEventListener('click', (event) => {
+        if (event.target.nodeName === "IMG") {
+            refs.modal.classList.add('is-open');
+            console.dir(event.target);
+            refs.modalImg.src = event.target.dataset.source;
+            refs.modalImg.alt = event.target.alt;
+        }
+        return;
+        
+    });
+        refs.closeBtn.addEventListener('click', () => {
+        refs.modal.classList.remove('is-open');
+        refs.modalImg.src = '';
+        refs.modalImg.alt = '';
+    });
+
+
+
+gallery.forEach(element => {
+const img = document.createElement('img');
+img.classList.add('gallery__image');
+img.src = element.preview;
+img.setAttribute('data-source', element.original);
+img.alt = element.description;
+
+const link = document.createElement('a');
+link.classList.add('gallery__link');
+// link.href = element.original;
+link.append(img);
+
+const li = document.createElement('li');
+li.classList.add('gallery__item');
+li.append(link);
+
+refs.gallery.append(li);
+})
+
